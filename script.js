43b06790ffd95bb873ad592639c3694ae8abb601
Feng Wang <fwang@tpnyc.local>
2018-09-07 16:18:07 +0800

    Initial import

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿document.getElementById("title01").innerHTML =
+document.getElementById("title01").text + " - Force Rock WONG";
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " FRW. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.htm'>Home</a></li>" +
+"<li><a href='games.htm'>Games</a></li>" +
+"<li><a href='live_pages.htm'>Live Pages</a></li>" +
+"<li><a href='software.htm'>Software</a></li>" +
+"<li><a href='about.htm'>About</a></li>" +
+"</ul>";
